86dad05e1d4967361b9d285a70e5d957f5196443
Evgeniia <zhenyamestnik@gmail.com>
2023-04-18 17:47:50 +0700

    232. Implement Queue using Stacks

---
+++ C++/232._Implement_Queue_using_Stacks.cpp
@@ -0,0 +1,47 @@
+class MyQueue {
+    stack<int> helper;
+    stack<int> nums;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        while (!nums.empty())
+        {
+            helper.push(nums.top());
+            nums.pop();
+        }
+
+        nums.push(x);
+
+        while (!helper.empty())
+        {
+            nums.push(helper.top());
+            helper.pop();
+        }
+    }
+    
+    int pop() {
+        const auto top = nums.top();
+        nums.pop();
+        return top;
+    }
+    
+    int peek() {
+        return nums.top();
+    }
+    
+    bool empty() {
+        return nums.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */

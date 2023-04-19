73c120c2f1871cb77756e9d8b2de21562866c12e
Evgeniia <zhenyamestnik@gmail.com>
2023-04-19 10:45:48 +0700

    933. Number of Recent Calls

---
+++ C++/933._Number_of_Recent_Calls.cpp
@@ -0,0 +1,20 @@
+class RecentCounter {
+    queue<int> q;
+public:
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        q.push(t);
+        while (q.back() - 3000 > q.front())
+            q.pop();
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */

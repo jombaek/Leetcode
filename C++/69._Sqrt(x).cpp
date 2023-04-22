df2ef11bd12e51e2bd5a39b6f7acd18f304ee5a8
Evgeniia <zhenyamestnik@gmail.com>
2023-04-22 15:03:34 +0700

    69. Sqrt(x)

---
+++ C++/69._Sqrt(x).cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x)
+    {
+        if (x == 0)
+            return 0;
+
+        int l = 1;
+        int r = x;
+
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+
+            if (mid <= x / mid && (mid + 1) > x / (mid + 1))
+                return mid;
+            else if (mid > x / mid)
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+
+        return l;
+    }
+};

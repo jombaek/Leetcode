15d2b0d2b2fd76e5276b2e7fd54afe20adb976be
Evgeniia <zhenyamestnik@gmail.com>
2023-04-24 15:13:08 +0700

    53. Maximum Subarray

--- C++/53._Maximum_Subarray.cpp
+++ C++/53._Maximum_Subarray.cpp
@@ -20,3 +20,25 @@ public:
         return maxSum;
     }
 };
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums)
+    {
+        int n = nums.size();
+        vector<int> dp(n);
+        int maxSum = nums[0];
+
+        dp[0] = nums[0];
+        
+        for (int i = 1; i < n; ++i)
+        {
+            dp[i] = max(dp[i -1] + nums[i], nums[i]);
+
+            if (dp[i] > maxSum)
+                maxSum = dp[i];
+        }
+
+        return maxSum;
+    }
+};

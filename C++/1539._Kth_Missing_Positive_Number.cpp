f45fbf8584ba4aed258815e137af8e60a5768781
Evgeniia <zhenyamestnik@gmail.com>
2023-04-24 15:05:22 +0700

    1539. Kth Missing Positive Number

---
+++ C++/1539._Kth_Missing_Positive_Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k)
+    {
+        int l = 0;
+        int r = arr.size();
+
+        while (l < r)
+        {
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid] - 1 - mid >= k)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+
+        return l + k;
+    }
+};

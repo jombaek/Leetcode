508dc09561f927fc54afd83de3b2adb9d26a2ddd
Evgeniia <zhenyamestnik@gmail.com>
2023-04-26 11:38:09 +0700

    121. Best Time to Buy and Sell Stock

--- C++/121._Best_Time_to_Buy_and_Sell_Stock.cpp
+++ C++/121._Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -19,3 +19,20 @@ public:
         return maxProfit;
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.size(); ++i)
+        {
+            minPrice = min(minPrice, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - minPrice);
+        }
+
+        return maxProfit;
+    }
+};

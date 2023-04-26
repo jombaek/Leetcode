467ad70283e55d2044b71ffe6db1f08ec941d7f8
Evgeniia <zhenyamestnik@gmail.com>
2023-04-26 23:25:10 +0700

    1174. Immediate Food Delivery II

--- SQL/1174._Immediate_Food_Delivery_II.sql
+++ SQL/1174._Immediate_Food_Delivery_II.sql
@@ -20,3 +20,13 @@ scheduled_count (scheduled) AS (
 
 SELECT ROUND(immediate / COUNT(delivery_id) * 100, 2) AS immediate_percentage
 FROM immediate_count, first_order_table;
+
+
+SELECT
+    ROUND(SUM(IF(order_date = customer_pref_delivery_date, 1, 0)) / COUNT(DISTINCT customer_id) * 100, 2) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date) AS first_order
+    FROM Delivery
+    GROUP BY customer_id
+);

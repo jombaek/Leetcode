5cec92744d655bf7047af47e43f24463fb231455
Evgeniia <zhenyamestnik@gmail.com>
2023-04-28 10:58:14 +0700

    350. Intersection of Two Arrays II

--- C++/350._Intersection_of_Two_Arrays_II.cpp
+++ C++/350._Intersection_of_Two_Arrays_II.cpp
@@ -1,3 +1,4 @@
+// Hash Table
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2)
@@ -22,5 +23,46 @@ public:
         }
 
         return ans;
+    }
+};
+
+// Binary Search
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2)
+    {
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<int> count(1001);
+        vector<int> res;
+
+        for (const auto& num : nums2)
+            count[num]++;
+
+        sort(nums2.begin(), nums2.end());
+
+        for (const auto& num : nums1)
+        {
+            int l = 0;
+            int r = m - 1;
+
+            while (l <= r)
+            {
+                int mid = l + (r - l) / 2;
+
+                if (nums2[mid] == num && count[num] > 0)
+                {
+                    res.push_back(num);
+                    count[num]--;
+                    break;
+                }
+                else if (nums2[mid] > num)
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+        }
+
+        return res;
     }
 };

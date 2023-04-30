10ed1d8f3829cbd52a951b6fbcabb21a71796b0c
Evgeniia <zhenyamestnik@gmail.com>
2023-04-30 11:06:14 +0700

    91. Decode Ways

--- C++/91._Decode_Ways.cpp
+++ C++/91._Decode_Ways.cpp
@@ -1,3 +1,4 @@
+// DP O(n) time and space
 class Solution {
 public:
     int numDecodings(string s)
@@ -21,5 +22,28 @@ public:
         }
 
         return dp[0];
+    }
+};
+
+// DP constant space
+class Solution {
+public:
+    int numDecodings(string s)
+    {
+        int n = s.size();
+        int prev = 1, prevPrev, cur;
+
+        for (int i = n - 1; i >= 0; --i)
+        {
+            cur = s[i] == '0' ? 0 : prev;
+
+            if (i < n - 1 && (s[i] == '1'|| s[i] == '2' && s[i + 1]< '7'))
+                cur += prevPrev;
+                
+            prevPrev = prev;
+            prev = cur;
+        }
+
+        return prev;
     }
 };

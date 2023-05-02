a0b81cfccca33cc27c8baddd5d02b3f3c54c7821
Evgeniia <zhenyamestnik@gmail.com>
2023-05-02 17:54:26 +0700

    695. Max Area of Island

--- C++/695._Max_Area_of_Island.cpp
+++ C++/695._Max_Area_of_Island.cpp
@@ -1,3 +1,4 @@
+// BFS
 class Solution {
 public:
     int bfs(vector<vector<int>>& grid, int r, int c)
@@ -49,6 +50,34 @@ public:
             for (int c = 0; c < n; ++c)
                 if (grid[r][c] == 1)
                     maxArea = max(maxArea, bfs(grid, r, c));
+
+
+        return maxArea;
+    }
+};
+
+// DFS
+class Solution {
+public:
+    int dfs(vector<vector<int>>& grid, int r, int c)
+    {
+        if (r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size() || grid[r][c] != 1)
+            return 0;
+
+        grid[r][c] = 0;
+        return 1 + dfs(grid, r - 1, c) + dfs(grid, r + 1, c) + dfs(grid, r, c - 1) + dfs(grid, r, c + 1);
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid)
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+        int maxArea = 0;
+
+        for (int r = 0; r < m; ++r)
+            for (int c = 0; c < n; ++c)
+                if (grid[r][c] == 1)
+                    maxArea = max(maxArea, dfs(grid, r, c));
 
 
         return maxArea;

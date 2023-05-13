0632ece11246e2d47aea9d59b2c281fa109b13aa
Evgeniia <zhenyamestnik@gmail.com>
2023-05-13 23:52:33 +0700

    58. Length of Last Word

---
+++ C++/58._Length_of_Last_Word.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s)
+    {
+        int ans = 0;
+        
+        for (int i = s.size() - 1; i >= 0; --i)
+        {
+            if (s[i] == ' ' && !ans)
+                continue;
+
+            if (s[i] == ' ')
+                break;
+
+            ++ans;
+        }
+
+        return ans;
+    }
+};

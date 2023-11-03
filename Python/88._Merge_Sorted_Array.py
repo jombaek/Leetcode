90dd1b7e5fe393e92d09da81db2b56f35e755c08
Evgeniia Mestnikova <zhenyamestnik@gmail.com>
2023-11-03 22:33:59 +0600

    improve solution

--- Python/88._Merge_Sorted_Array.py
+++ Python/88._Merge_Sorted_Array.py
@@ -3,16 +3,11 @@ class Solution:
         k = m + n - 1
         i, j = m - 1, n - 1
 
-        while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
                 i -= 1
             else:
                 nums1[k] = nums2[j]
                 j -= 1
             k -= 1
-        
-        while j >= 0:
-            nums1[k] = nums2[j]
-            k -= 1
-            j -= 1
